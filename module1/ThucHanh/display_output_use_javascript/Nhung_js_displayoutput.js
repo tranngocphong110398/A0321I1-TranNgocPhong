a9b276fc729db2ba870c43909106f797eed89abb
tranngocphong110398 <tranngocphong110398@gmail.com>
2021-04-08 23:53:51 +0700

    upload thuc hanh javascript

---
+++ module1/ThucHanh/display_output_use_javascript/Nhung_js_displayoutput.js
@@ -0,0 +1,12 @@
+function showthongbao() {
+    alert("xin chào");
+}
+function showMessage(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
+function showMessage2() {
+    document.write('xin chào = documentwite');
+}
+function showMessage3() {
+    console.log('xin chào = console.log');
+}
