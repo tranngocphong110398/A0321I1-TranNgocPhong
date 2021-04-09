538e27939fc24619f98a5f1b3d67d719f2f7c936
tranngocphong110398 <tranngocphong110398@gmail.com>
2021-04-09 18:41:12 +0700

    upload thuc hanh toan tu

---
+++ module1/ThucHanh/Toan_tu/nhung_js_toan_tu.js
@@ -0,0 +1,6 @@
+function tinhdientichhcn() {
+let width=parseInt(prompt("nhập vào chiều rộng hình chữ nhật"));
+let height=parseInt(prompt("nhập vào chiều dài hình chữ nhật"));
+let dientichhcn=width*height;
+alert("diện tích hình chữ nhật là "+dientichhcn);
+}
