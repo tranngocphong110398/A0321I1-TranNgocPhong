aafc786a791943f8bc4cbb99fe5c2d9e6ef146f5
tranngocphong110398 <tranngocphong110398@gmail.com>
2021-04-18 20:23:17 +0700

    upload Bai8_Luyen_Tap_Cau_Truc_IF_ELSE

---
+++ module1/ThucHanh/Bai8_Luyen_Tap_Cau_Truc_IF_ELSE/Nhung_js_Bai8_Luyen_Tap_Cau_Truc_IF_ELSE.js
@@ -0,0 +1,8 @@
+alert("What is the official name of javascript");
+var a=prompt("input answer for you");
+if (a==="ECMAScript"){
+    alert("Right");
+}
+else {
+    alert("Didn’t know? ECMAScript!");
+}
