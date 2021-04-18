f7aad422fe5dbacafa543f0c82c3bfdc6a253905
tranngocphong110398 <tranngocphong110398@gmail.com>
2021-04-18 21:02:15 +0700

    upload Bai8_Luyen_Tap_Toan_Tu_Ba_Ngoi

---
+++ module1/ThucHanh/Bai8_Luyen_Tap_Toan_Tu_Ba_Ngoi/Nhung_Bai8_Luyen_Tap_Toan_Tu_Ba_Ngoi.js
@@ -0,0 +1,5 @@
+var a=parseInt(prompt("Enter any number"));
+var b=parseInt(prompt("Enter any number"));
+result=(a+b<4) ? alert("Below") : alert("Over");
+var message=prompt("Enter value for message");
+message=="Employee" ? alert("Hello") : message=="Director" ? alert("Greetings"): message=="" ? alert("LOGIN"): alert("");
